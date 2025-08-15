9dcec242a3529c0d78c6b4da4593ecd2c26bde72
Haroon <81903831+Dawood-Siddique@users.noreply.github.com>
2025-08-16 01:47:07 +0500

    combobox added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-function App() {
+export default function App() {
   return (
     <div>
       <h1>Main Page</h1>
     </div>
   );
 }
-
-export default App;
--- src/pages/AddPolicy.tsx
+++ src/pages/AddPolicy.tsx
@@ -1,8 +1,89 @@
+import { Input } from "@/components/ui/input"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 
-export default function AddPolicy () {
+import { ComboboxDemo } from "@/components/ui/combobox"
+
+export default function AddPolicy() {
     return (
-        <div>
-            <h1>Add Policy</h1>
+        <div className="flex justify-center h-screen ">
+            <div className="flex-row">
+                <h1>Policy Management</h1>
+                <div className="flex flex-row gap-4">
+                    <div>
+                        Date
+                        <Input type="date" />
+                    </div>
+                    <div>
+                        Insurance Company
+                        <div>
+                            <ComboboxDemo />
+                        </div>
+                        {/* <Select>
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder="Theme" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="light">Light</SelectItem>
+                                <SelectItem value="dark">Dark</SelectItem>
+                                <SelectItem value="system">System</SelectItem>
+                            </SelectContent>
+                        </Select> */}
+                    </div>
+                </div>
+
+                <div className="flex flex-row gap-4">
+                    <div>
+                        Policy Number
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        Insured Name
+                        <Select>
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder="Theme" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="light">Light</SelectItem>
+                                <SelectItem value="dark">Dark</SelectItem>
+                                <SelectItem value="system">System</SelectItem>
+                            </SelectContent>
+                        </Select>
+
+                    </div>
+                </div>
+
+                <div className="flex flex-row gap-4">
+                    <div>
+                        Car Model
+                        <Input type="text" />
+                    </div>
+                    <div>
+                        Engine Type
+                        <Input type="text" />
+                    </div>
+                </div>
+
+                <div>
+                    Agent Name
+                    <Select>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Theme" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="light">Light</SelectItem>
+                            <SelectItem value="dark">Dark</SelectItem>
+                            <SelectItem value="system">System</SelectItem>
+                        </SelectContent>
+                    </Select>
+
+                </div>
+            </div>
         </div>
     )
 }
