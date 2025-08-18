7ffeea03edba63bfc0a2e6d7788cdd02ac82d522
Haroon <81903831+Dawood-Siddique@users.noreply.github.com>
2025-08-19 00:42:11 +0500

    complete add policy ui

--- src/pages/AddPolicy.tsx
+++ src/pages/AddPolicy.tsx
@@ -1,21 +1,48 @@
 import { Input } from "@/components/ui/input"
 import { Combobox } from "@/components/ui/combobox"
 import { useState } from "react"
+import { Button } from "@/components/ui/button"
 
-const insuranceCompanies = [
+const insuranceCompanyObj = [
     { value: "allianz", label: "Allianz" },
     { value: "axa", label: "AXA" },
     { value: "generali", label: "Generali" },
     { value: "zurich", label: "Zurich" },
 ]
 
+const paymentMethodObj = [
+    { value: 'cash', label: 'cash' },
+    { value: 'bank', label: 'bank' }
+]
+
+const paymentStatusObj = [
+    { value: 'active', label: 'active' },
+    { value: 'complete', label: 'complete' },
+    { value: 'cancelled', label: 'cancelled' }
+]
+
+const agentNameObj = [
+    { value: 'agent1', label: 'Agent 1' },
+    { value: 'agent2', label: 'Agent 2' },
+]
+
+const insurdNameObj = [
+    { value: 'insurd1', label: 'Insurd 1' },
+    { value: 'insurd2', label: 'Insurd 2' },
+]
+
 export default function AddPolicy() {
-    const [company, setCompany] = useState(" ")
+    const [company, setCompany] = useState("")
+    const [payment, setPayment] = useState("")
+    const [paymentStatus, setPaymentStatus] = useState("")
+    const [agentName, setAgentName] = useState("")
+    const [insuredName, setInsuredName] = useState("")
+
 
     return (
         <div className="flex justify-center h-screen ">
             <div className="flex-row">
-                <h1>Policy Management</h1>
+                <div className="text-2xl font-bold">Policy Management</div>
                 <div className="flex flex-row gap-4">
                     <div>
                         Date
@@ -25,7 +52,7 @@ export default function AddPolicy() {
                         Insurance Company
                         <div>
                             <Combobox
-                                items={insuranceCompanies}
+                                items={insuranceCompanyObj}
                                 value={company}
                                 onChange={setCompany}
                                 placeholder="Select company..."
@@ -43,7 +70,16 @@ export default function AddPolicy() {
                     </div>
                     <div>
                         Insured Name
-                        <Input type="text" />
+                        <div>
+                            <Combobox
+                                items={insurdNameObj}
+                                value={insuredName}
+                                onChange={setInsuredName}
+                                placeholder="Select insured..."
+                                searchPlaceholder="Search insured..."
+                                noResultsMessage="No insured found."
+                            />
+                        </div>
                     </div>
                 </div>
 
@@ -60,9 +96,94 @@ export default function AddPolicy() {
 
                 <div>
                     Agent Name
-                    <Input type="text" />
+                    <div>
+                        <Combobox
+                            items={agentNameObj}
+                            value={agentName}
+                            onChange={setAgentName}
+                            placeholder="Select Agent..."
+                            searchPlaceholder="Search Agent..."
+                            noResultsMessage="No Agent found."
+                        />
+                    </div>
+
+                </div>
+
+                {/* Payment Information */}
+
+                <div>
+                    <div className="text-2xl font-bold">Payment Information</div>
+                    <div className="flex flex-row gap-4">
+                        <div>
+                            Gross/Price
+                            <Input type="text" />
+                        </div>
+                        <div>
+                            New CO Rates
+                            <Input type="text" />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-row gap-4">
+                        <div>
+                            Client Price
+                            <Input type="text" />
+                        </div>
+                        <div>
+                            Profit
+                            <Input type="text" />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-row gap-4">
+                        <div>
+                            Payment Method
+                            <div>
+                                <Combobox
+                                    items={paymentMethodObj}
+                                    value={payment}
+                                    onChange={setPayment}
+                                    placeholder="Select Method..."
+                                    searchPlaceholder="Search Method"
+                                    noResultsMessage="No Method found."
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            Payment Status
+                            <div>
+                                <Combobox
+                                    items={paymentStatusObj}
+                                    value={paymentStatus}
+                                    onChange={setPaymentStatus}
+                                    placeholder="Select Status..."
+                                    searchPlaceholder="Search Status"
+                                    noResultsMessage="No Status found."
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        Remarks
+                        <Input type="text" />
+                    </div>
+
+                    <div>
+                        Reference Number
+                        <Input type="text" />
+                    </div>
+
+                </div>
+
+                {/* Cancel and Save Button */}
+                <div className="mt-4">
+                    <Button variant="secondary">Cancel</Button>
+                    <Button>Save</Button>
                 </div>
             </div>
+
         </div>
     )
 }
