5fdb85d5018530ab5fe680711cc679d0c100d352
Haroon <81903831+Dawood-Siddique@users.noreply.github.com>
2025-08-30 17:34:41 +0500

    feat(policies): add stats section to policies page

--- src/pages/Policies.tsx
+++ src/pages/Policies.tsx
@@ -5,7 +5,12 @@ export default function Policies() {
   return (
     <div>
       <Header />
-      <div>Policies Page</div>
+      <main className="container mx-auto mt-8 bg-amber-400" >
+        <div className="text-4xl font-bold">Policies Stat</div>
+        <div>
+          
+        </div>
+      </main>
     </div>
   )
 }
