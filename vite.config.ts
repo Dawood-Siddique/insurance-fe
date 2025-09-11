8f6940f7d27ac67f4193b290cc0b7b8047e05672
Dawood Siddique <81903831+Dawood-Siddique@users.noreply.github.com>
2025-09-11 13:53:39 +0500

    file issue resolved

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 
 // https://vite.dev/config/
@@ -12,7 +12,12 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true,
     },
   },
 })
